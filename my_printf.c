e1d5604696870e94c46c9cf0e8e6b360752b3b7e
louimagona <louimagona2@gmail.com>
2021-07-27 08:18:12 -0700

    modified

--- my_printf.c
+++ my_printf.c
@@ -1,40 +1,43 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "holberton.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * _printf - my implementation of the printf function
- * @format: character that specifies the format
+ * _printf - prints just like printf
+ * @format: character to specify printing format
+ * Return: number of parameters passed
  */
 
-int _printf(const char *format,...)
+int _printf(const char *format, ...)
 {
-    char *s;
-    char *t;
-    int i;
-    va_list ap;
+	const char *iter;
+	char *s;
+	unsigned int i;
+	va_list ap;
 
-    va_start(ap, format);
-    for (t = format; *t != '\0'; t++)
-    {
-        while (*t != '%')
-        {
-            putchar(*t);
-            t++;
-        }
-        t++;
-        
-        switch(*t)
-        {
-            case 'c': 
-            i = va_arg(ap, int);
-            putchar(i);
-            break;
-            case 's':
-            s = va_arg(ap, char *);
-            puts(s);
-            break;
-        }
-        va_end(ap);
-    }
+	va_start(ap, format);
+
+	for (iter = format; *iter != '\0'; iter++)
+	{
+		while (*iter != '%')
+		{
+			putchar(*iter);
+			iter++;
+		}
+		iter++;
+
+		switch (*iter)
+		{
+			case 'c':
+				i = va_arg(ap, int);
+				putchar(i);
+				break;
+			case 's':
+				s = va_arg(ap, char*);
+				puts(s);
+				break;
+		}
+		va_end(ap);
+	}
+	return (i);
 }
